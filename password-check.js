9b4d53b06362c63e22f193919908023ac7697237
sehar-kamran <83481003+sehar-kamran@users.noreply.github.com>
2021-05-07 13:38:33 -0400

    Add files via upload

---
+++ password-check.js
@@ -0,0 +1,19 @@
+function checkforblank() {
+        
+    if (document.getElementById('user').value == "") {
+        alert('Please provide a valid username.');
+        return false;
+    } else if (document.getElementById('password').value == "") {
+        alert('Please provide a valid password.');
+        return false;
+    } else if (document.getElementById('user').value == 'sehar.k'){
+        if (document.getElementById('password').value == 'kamran'){
+            window.setTimeout(function(){
+
+// Correct password redirect. How did you find me?
+window.location.href = "https://sehar-login.danbotdev.repl.co/loading.html";
+
+}, 1);
+        } else return alert('Incorrect username or password.')
+    } else return alert('Incorrect username or password.')
+}
